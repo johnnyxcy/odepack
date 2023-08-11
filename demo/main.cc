d6efeba716ac0016994596cdb3eadd49da5cf9be
johnnyxcy <johnny.xcy1997@outlook.com>
2023-08-11 14:12:39 +0800

    Fix: remove dverk to seperate pacakge

--- demo/main.cc
+++ demo/main.cc
@@ -4,8 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "odepack/dverk.h"
-#include "odepack/odepack.h"
+#include "odepack/dlsoda.h"
 
 using namespace std;
 
@@ -15,9 +14,19 @@ struct PkData {
   double ka;
 };
 
+// int pk(int *neq, double *t, double *y, double *ydot, void *dat) {
+
+//   auto pk_data = *static_cast<PkData *>(dat);
+//   // auto pk_data = PkData{.cl = 0.134, .v = 7.81, .ka = 0.571};
+
+//   ydot[0] = -pk_data.ka * y[0];
+//   ydot[1] = pk_data.ka * y[0] - pk_data.cl / pk_data.v * y[1];
+//   return (0);
+// }
+
 int pk(int *neq, double *t, double *y, double *ydot) {
 
-  // auto pk_data = static_cast<PkData *>(dat);
+  // auto pk_data = *static_cast<PkData *>(dat);
   auto pk_data = PkData{.cl = 0.134, .v = 7.81, .ka = 0.571};
 
   ydot[0] = -pk_data.ka * y[0];
@@ -70,63 +79,26 @@ int test_lsoda() {
   return (0);
 }
 
-// int test_dverk() {
-//   int neq, itol, ind, nw;
-//   double tol, t, tout, y[2], c[40];
-
-//   nw = 10;
-//   double **w = new double *[nw];
-//   for (int i = 0; i < nw; i++)
-//     w[i] = new double[9];
-
-//   neq = 2;
-//   t = 0.0E0;
-//   tout = 0.1;
-//   tol = 1.0E-6;
-//   ind = 1;
-
-//   y[0] = 100.0;
-//   y[1] = 0.0;
-
-//   c[1] = 5;
-
-//   for (int i = 0; i < 12; i++) {
-//     dverk(&neq, pk, &t, y, &tout, &tol, &ind, c, &nw, w);
-//     if (ind == 3) {
-//       printf(" at t= %12.4e y= %14.6e %14.6e\n", t, y[0], y[1]);
-//       ind = 1;
-//     } else {
-//       break;
-//     }
-//     tout = tout + i;
-//   }
-
-//   // cleanup:
-//   for (int i = 0; i < nw; i++)
-//     delete[] w[i];
-//   delete[] w;
-
-//   return (0);
-// }
 int main(void) {
   chrono::steady_clock::time_point begin, end;
   begin = chrono::steady_clock::now();
   double dt;
 
   int N = 1;
+
+  // for (int i = 0; i < N; i++) {
+  //   test_lsoda();
+  // }
+  // end = chrono::steady_clock::now();
+  // dt = chrono::duration_cast<chrono::microseconds>(end - begin).count();
+  // cout << "Time taken (us/per loop) = " << dt / N << endl;
+
   for (int i = 0; i < N; i++) {
     test_lsoda();
   }
   end = chrono::steady_clock::now();
   dt = chrono::duration_cast<chrono::microseconds>(end - begin).count();
   cout << "Time taken (us/per loop) = " << dt / N << endl;
-
-  // for (int i = 0; i < N; i++) {
-  //   test_dverk();
-  // }
-  // end = chrono::steady_clock::now();
-  // dt = chrono::duration_cast<chrono::microseconds>(end - begin).count();
-  // cout << "Time taken (us/per loop) = " << dt / N << endl;
 
   return (0);
 }
